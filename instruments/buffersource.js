4b7e8b49acc91a3a0dc2d578e7da47c7b9836da5
Ari Asulin <ari.asulin@gmail.com>
2018-10-09 11:24:46 -0700

    buffer source refactor

--- instruments/buffersource.js
+++ instruments/buffersource.js
@@ -3,28 +3,39 @@
 (function() {
 
 
-    // var real = new Float32Array(2);
-    // var imag = new Float32Array(2);
-    // var ac = new AudioContext();
-    // var osc = ac.createOscillator();
-    //
-    // real[0] = 0;
-    // imag[0] = 0;
-    // real[1] = 1;
-    // imag[1] = 0;
-    //
-    // var wave = ac.createPeriodicWave(real, imag, {disableNormalization: true});
-    //
-    // osc.setPeriodicWave(wave);
-    //
-    // osc.connect(ac.destination);
-    //
-    // osc.start();
-    // osc.stop(2);
+
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+// Create an empty three-second stereo buffer at the sample rate of the AudioContext
+    var myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
+
+// Fill the buffer with white noise;
+//just random values between -1.0 and 1.0
+    for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+        // This gives us the actual ArrayBuffer that contains the data
+        var nowBuffering = myArrayBuffer.getChannelData(channel);
+        for (var i = 0; i < myArrayBuffer.length; i++) {
+            // Math.random() is in [0; 1.0]
+            // audio needs to be in [-1.0; 1.0]
+            nowBuffering[i] = Math.random() * 2 - 1;
+        }
+    }
+
+// Get an AudioBufferSourceNode.
+// This is the AudioNode to use when we want to play an AudioBuffer
+    var source = audioCtx.createBufferSource();
+// set the buffer in the AudioBufferSourceNode
+    source.buffer = myArrayBuffer;
+// connect the AudioBufferSourceNode to the
+// destination so we can hear the sound
+    source.connect(audioCtx.destination);
+// start the source playing
+    source.start();
 
 
 
-    class OscillatorSimple {
+
+    class BufferSource {
         constructor(preset, id) {
             this.id = id;
             this.preset = preset;
@@ -77,7 +88,7 @@
         window.instruments = {};
     if(!window.instruments['snesology.net'])
         window.instruments['snesology.net'] = {};
-    window.instruments['snesology.net']['/instruments/oscillator.js'] = OscillatorSimple;
+    window.instruments['snesology.net']['/instruments/buffersource.js'] = BufferSource;
     window.instruments['localhost'] = window.instruments['snesology.net']; // For local debugging
 
     // instrument
