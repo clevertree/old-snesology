1dbf3dea264308582b07f33babe287d806966f91
Ari Asulin <Ari.Asulin@gmail.com>
2018-10-18 18:40:58 -0700

    refactor websockets

--- server/server.js
+++ server/server.js
@@ -1,32 +1,10 @@
 const express = require('express');
 const path = require('path');
 // const redis = require("redis");
-let app;
+const BASE_DIR = path.dirname(__dirname);
 
 // Init
 module.exports = function(appInstance, router) {
-    app = appInstance;
-    const BASE_URL = path.dirname(__dirname);
-
     // Serve Website Files
-    router.use('/', express.static(BASE_URL));
-
-    // Web Socket
-    router.ws('*', handleWebSocketRequest);
-
-    app.addWebSocketListener = addWebSocketListener;
+    router.use('/', express.static(BASE_DIR));
 };
-
-const webSocketListeners = [];
-function addWebSocketListener(callback) {
-    webSocketListeners.push(callback);
-}
-
-function handleWebSocketRequest(ws, req) {
-    console.info("WS connection: ", req.headers["user-agent"]);
-    for(let i=0; i<webSocketListeners.length; i++) {
-        const listener = webSocketListeners[i];
-        listener(ws, req);
-    }
-}
-
