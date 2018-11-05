57c19a64ff62e6f498e68afd8f4d2726d61746cd
Ari Asulin <ari.asulin@gmail.com>
2018-11-04 16:07:22 -0800

    buffer source refactor

--- instrument/audiosource/buffersource.js
+++ instrument/audiosource/buffersource.js
@@ -4,85 +4,12 @@
 
 
 
-    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-// Create an empty three-second stereo buffer at the sample rate of the AudioContext
-    var myArrayBuffer = audioCtx.createBuffer(2, audioCtx.sampleRate * 3, audioCtx.sampleRate);
-
-// Fill the buffer with white noise;
-//just random values between -1.0 and 1.0
-    for (var channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
-        // This gives us the actual ArrayBuffer that contains the data
-        var nowBuffering = myArrayBuffer.getChannelData(channel);
-        for (var i = 0; i < myArrayBuffer.length; i++) {
-            // Math.random() is in [0; 1.0]
-            // audio needs to be in [-1.0; 1.0]
-            nowBuffering[i] = Math.random() * 2 - 1;
-        }
-    }
-
-// Get an AudioBufferSourceNode.
-// This is the AudioNode to use when we want to play an AudioBuffer
-    var source = audioCtx.createBufferSource();
-// set the buffer in the AudioBufferSourceNode
-    source.buffer = myArrayBuffer;
-// connect the AudioBufferSourceNode to the
-// destination so we can hear the sound
-    source.connect(audioCtx.destination);
-// start the source playing
-    source.start();
-
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('GET', '/path/to/image.png', true);
-
-    //
-    // request.open('GET', 'viper.ogg', true);
-    //
-    // request.responseType = 'arraybuffer';
-    //
-    //
-    // request.onload = function() {
-    //     var audioData = request.response;
-    //
-    //     audioCtx.decodeAudioData(audioData, function(buffer) {
-    //             source.buffer = buffer;
-    //
-    //             source.connect(audioCtx.destination);
-    //             source.loop = true;
-    //         },
-    //
-    //         function(e){ console.log("Error with decoding audio data" + e.err); });
-    //
-    // }
-
-
-    /*
-      This audio context is unprefixed!
-    */
-    var context = new AudioContext();
-
-    var audioSource = context.createBufferSource();
-    audioSource.connect( context.destination );
-
-    context.decodeAudioData( buffer, function( res ) {
-
-        audioSource.buffer = res;
-
-        /*
-           Do something with the sound, for instance, play it.
-           Watch out: all the sounds will sound at the same time!
-        */
-        audioSource.noteOn( 0 );
-
-    } );
-
-
-
     class BufferSource {
         constructor(preset, id) {
             this.id = id;
             this.preset = preset;
             this.config = preset.config || {};            // TODO: validate config
+            this.samples = {};
         }
 
         play(destination, frequency, startTime, duration) {
@@ -116,6 +43,41 @@
                 </form>
             `
         };
+
+
+        loadAudioSample(context, onLoad, sampleURL) {
+            const request = new XMLHttpRequest();
+
+            request.open('GET', sampleURL, true);
+            request.responseType = 'arraybuffer';
+
+            request.onload = function() {
+                var audioData = request.response;
+
+                context.decodeAudioData(audioData,
+                    (buffer) => {
+                        this.samples[sampleURL] = buffer;
+                        // const source = context.createBufferSource();
+                        // const myBuffer = buffer;
+                        // const songLength = buffer.duration;
+                        // source.buffer = myBuffer;
+                        // source.playbackRate.value = playbackControl.value;
+                        // source.connect(context.destination);
+                        // source.loop = true;
+                        // onLoad(null, source);
+
+                        // loopstartControl.setAttribute('max', Math.floor(songLength));
+                        // loopendControl.setAttribute('max', Math.floor(songLength));
+                    },
+
+                    (e) => {
+                        onLoad("Error with decoding audio data" + e.error, null);
+                    }
+                );
+            };
+
+            request.send();
+        }
 
         getNamedFrequency (frequencyName) {
             switch(frequencyName) {
