81e3f881a11a63e8a096e4e60bbca75a2c622ab4
Ari Asulin <ari.asulin@gmail.com>
2019-12-18 17:45:50 -0700

    player/editor updates

---
+++ .scripts/tracker-server.js
@@ -0,0 +1,74 @@
+var {Server} = require('bittorrent-tracker');
+
+const port = 8008;
+const hostname = null;
+
+var server = new Server({
+    udp: true, // enable udp server? [default=true]
+    http: true, // enable http server? [default=true]
+    ws: true, // enable websocket server? [default=true]
+    stats: true, // enable web-based statistics? [default=true]
+    filter: function (infoHash, params, cb) {
+        // Blacklist/whitelist function for allowing/disallowing torrents. If this option is
+        // omitted, all torrents are allowed. It is possible to interface with a database or
+        // external system before deciding to allow/deny, because this function is async.
+
+        // It is possible to block by peer id (whitelisting torrent clients) or by secret
+        // key (private trackers). Full access to the original HTTP/UDP request parameters
+        // are available in `params`.
+
+        // This example only allows one torrent.
+
+        var allowed = (infoHash === '005ff6b3e47f34ad254b301481561d3145187467');
+        if (allowed) {
+            // If the callback is passed `null`, the torrent will be allowed.
+            cb(null)
+        } else {
+            // If the callback is passed an `Error` object, the torrent will be disallowed
+            // and the error's `message` property will be given as the reason.
+            cb(new Error('disallowed torrent'))
+        }
+    }
+});
+
+server.on('error', function (err) {
+    // fatal server error!
+    console.log(err.message)
+});
+
+server.on('warning', function (err) {
+    // client sent bad data. probably not a problem, just a buggy client.
+    console.log(err.message)
+});
+
+server.on('listening', function () {
+    // fired when all requested servers are listening
+    console.log('listening on http port:' + server.http.address().port);
+    console.log('listening on udp port:' + server.udp.address().port);
+    console.log('listening on ws port:' + server.ws.address().port);
+});
+
+// start tracker server listening! Use 0 to listen on a random free port.
+server.listen(port, hostname);
+
+// listen for individual tracker messages from peers:
+
+server.on('start', function (addr) {
+    console.log('got start message from ' + addr)
+});
+
+server.on('complete', function (addr) {});
+server.on('update', function (addr) {});
+server.on('stop', function (addr) {});
+
+// // get info hashes for all torrents in the tracker server
+// Object.keys(server.torrents);
+//
+// // get the number of seeders for a particular torrent
+// server.torrents[infoHash].complete;
+//
+// // get the number of leechers for a particular torrent
+// server.torrents[infoHash].incomplete;
+//
+// // get the peers who are in a particular torrent swarm
+// server.torrents[infoHash].peers;
